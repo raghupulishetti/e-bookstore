a77235d6ae69731ce8cd32253316a0f2f0f75238
raghupulishetti <raghupulishetti@gmail.com>
2022-01-29 18:27:02 +0530

    Adding user service

---
+++ user-service/src/test/java/com/dailycoder/bookstore/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycoder.bookstore.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
