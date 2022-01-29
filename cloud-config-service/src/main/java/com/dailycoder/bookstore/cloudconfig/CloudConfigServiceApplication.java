6c365c8e667d6e6cb11a09eaa597f557b04f781e
raghupulishetti <raghupulishetti@gmail.com>
2022-01-29 11:20:51 +0530

    added classes

---
+++ cloud-config-service/src/main/java/com/dailycoder/bookstore/cloudconfig/CloudConfigServiceApplication.java
@@ -0,0 +1,16 @@
+package com.dailycoder.bookstore.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServiceApplication.class, args);
+	}
+
+}
